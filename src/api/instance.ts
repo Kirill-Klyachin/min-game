7ae4e8ad972dc2f5e983300bdfa1b704066e32b9
Kirill-Klyachin <MrKiryn@yandex.ru>
2024-02-29 15:22:48 +0500

    Fixed api url

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const instance = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:4000/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
